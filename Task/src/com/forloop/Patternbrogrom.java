40e54c0f87d31a9e53d52b10da04c7a46b09be10
sentamil <selvansenthamil095.com>
2022-11-17 09:55:33 +0530

    for loop pattern

---
+++ Task/src/com/forloop/Patternbrogrom.java
@@ -0,0 +1,41 @@
+package com.forloop;
+
+public class Patternbrogrom {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Patternbrogrom pb=new Patternbrogrom();
+		//pb.print12345();
+		pb.print1234();
+	}
+
+	private void print1234() {
+		// TODO Auto-generated method stub
+		
+		for(int row=5; row>=1; row--)
+		{
+			for(int num=5; num>=row; num--) 
+			{
+				System.out.print(num+" ");
+			
+			}
+		System.out.println();
+		}
+	}
+
+	private void print12345() {
+		// TODO Auto-generated method stub
+		
+		for(int row=1; row<=5; row++)
+		{
+			for(int num=1; num<=6-row; num++) 
+			{
+				System.out.print(num+" ");
+				
+			}
+		System.out.println();
+		}
+	}
+
+}
