5e6e3c64829c3ea1c4a3f0ebfe86e10caaccd171
sentamil <selvansenthamil095.com>
2022-11-17 11:19:03 +0530

    my try

--- Task/src/com/forloop/Patternbrogrom.java
+++ Task/src/com/forloop/Patternbrogrom.java
@@ -6,8 +6,54 @@ public class Patternbrogrom {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Patternbrogrom pb=new Patternbrogrom();
-		///pb.print12345();/
+		pb.print12345();//
 		pb.print1234();
+		pb.print3();
+		pb.print1();
+		pb.print2();
+	}
+
+	private void print2() {
+		// TODO Auto-generated method stub
+		for(int row=1;row<=5;row++) 
+		{
+			for(int col=1;col<=6-row;col++) 
+			{
+				System.out.print(1+" ");
+			}System.out.println();
+		}
+		
+	}
+
+	private void print1() {
+		// TODO Auto-generated method stub
+		for(int row=0;row<=4;row++) {
+			for(int col=1;col<=5-row;col++) {
+				System.out.print((row+col)+" ");
+			}
+			System.out.println();
+		}
+	}
+
+	private void print5() {
+		// TODO Auto-generated method stub
+		for(int row=5;row>=1;row--) {
+			for(int col=5;col>=6-row;col--) {
+				System.out.print(row+" ");
+		}
+		System.out.println();}
+	}
+
+	private void print3() {
+		// TODO Auto-generated method stub
+		for(int row=1;row<=5;row++)
+		{
+			for(int num=1;num<=6-row; num++) {
+				System.out.print(num+" ");
+			}
+			System.out.println();
+		}
+		
 	}
 
 	private void print1234() {
@@ -31,7 +77,7 @@ public class Patternbrogrom {
 		{
 			for(int num=1; num<=6-row; num++) 
 			{
-				System.out.print(num+" ");
+				System.out.print(1+" ");
 				
 			}
 		System.out.println();
