aa87ea2d3c18535027930e949c70b664d554ca97
sentamil <selvansenthamil095.com>
2022-11-17 10:09:19 +0530

    loop

---
+++ Task/src/com/basic/loop/LoopCode.java
@@ -0,0 +1,16 @@
+package com.basic.loop;
+
+public class LoopCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number=10;
+		while(number>=1) {
+			System.out.print(number+" ");
+			number=number-1;//number++
+		}
+
+		
+	}
+
+}
