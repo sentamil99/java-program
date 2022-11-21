ad00d4f65257319b891ea035cde36d8955f55eab
sentamil99 <118245575+sentamil99@users.noreply.github.com>
2022-11-21 11:47:59 +0530

    Update LoopCode.java

--- Task/src/com/basic/loop/LoopCode.java
+++ Task/src/com/basic/loop/LoopCode.java
@@ -3,7 +3,7 @@ package com.basic.loop;
 public class LoopCode {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	//	// TODO Auto-generated method stub
 		int number=10;
 		while(number>=1) {
 			System.out.print(number+" ");
