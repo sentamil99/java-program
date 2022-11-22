1f90e1c7d0b25a7a98f0d6fb2d951ba5cdf848f6
sentamil <selvansenthamil095.com>
2022-11-22 17:07:29 +0530

    array

---
+++ Task/src/com/array/ArrayTask.java
@@ -0,0 +1,24 @@
+package com.array;
+
+public class ArrayTask {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a=new int[5];
+		int[] b= {10,20,30};
+		a[0]=10;
+		a[1]=20;
+		a[2]=30;
+		a[3]=30;
+		a[4]=50;
+		for(int i=0;i<a.length;i++) {
+			System.out.println(a[i]);
+		}
+		
+		/*
+		 * System.out.println(a[4]); System.out.println(b[2]);
+		 */
+		
+	}
+
+}
